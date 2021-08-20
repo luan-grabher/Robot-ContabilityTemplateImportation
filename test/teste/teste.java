5669c92227f6bb1192f4c3fc34ab57e3900ed02e
Luan <Luan@DESKTOP-B4ERJ5H>
2021-08-20 16:34:48 -0300

    change test

--- test/teste/teste.java
+++ test/teste/teste.java
@@ -7,9 +7,9 @@ public class teste {
     public static void main(String[] args) {
         StringBuilder parametros = new StringBuilder();
         
-        parametros.append("[mes:1]");
+        parametros.append("[mes:6]");
         parametros.append("[ano:2021]");
-        parametros.append("[ini:robot-mottin386]");
+        parametros.append("[ini:robot-pantano]");
 
         Main.testParameters = parametros.toString();
         args = new String[]{"test"};
